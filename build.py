7707e33bee67bc1adb9a06055de888d35151f925
Emanuele Gissi <emanuele.gissi@gmail.com>
2025-08-22 01:26:06 +0200

    New script

---
+++ build.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+BLENDER = "../blender-4.5.0-linux-x64/blender"
+SOURCE_DIR = "./source"
+OUTPUT_DIR = "."
+CWD_PATH = "."
+
+import subprocess
+
+# blender --command extension build --source-dir SOURCE_DIR
+#                                   [--output-dir OUTPUT_DIR]
+#                                   [--output-filepath OUTPUT_FILEPATH]
+#                                   [--valid-tags VALID_TAGS_JSON]
+#                                   [--split-platforms] [--verbose]
+
+
+command = [
+    BLENDER,
+    "--command",
+    "extension",
+    "build",
+    "--source-dir",
+    SOURCE_DIR,
+    "--output-dir",
+    OUTPUT_DIR,
+    "--verbose",
+]
+
+print("Build...")
+res = subprocess.run(command, cwd=CWD_PATH, capture_output=True, text=True)
+output = res.stdout + res.stderr
+print(output)
